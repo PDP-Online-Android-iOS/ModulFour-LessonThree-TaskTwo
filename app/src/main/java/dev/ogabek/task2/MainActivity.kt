a356539c944e0a49cb84e60a5206ed1017fabc35
OgabekDev <ogabekdev@gmail.com>
2021-12-23 19:30:15 +0500

    Modul Four - Lesson Three - Task Two

--- app/src/main/java/dev/ogabek/task2/MainActivity.kt
+++ app/src/main/java/dev/ogabek/task2/MainActivity.kt
@@ -1,11 +1,28 @@
 package dev.ogabek.task2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+
     }
+
+    private fun initViews() {
+        val openBtn = findViewById<Button>(R.id.btn_open)
+        openBtn.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+
+    private fun openSecondActivity() {
+        startActivity(Intent(this, SecondActivity::class.java))
+    }
+
 }
---
+++ app/src/main/java/dev/ogabek/task2/SecondActivity.kt
@@ -0,0 +1,11 @@
+package dev.ogabek.task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
